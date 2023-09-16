a5a1258d0ec99cb47339bc838e039faf5011b841
Mai <mai.zakaria.18@gmail.com>
2023-09-16 05:58:22 -0700

    conversion specifiers

---
+++ task_1.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * print_string - prints a string
+ * @list: list of arguments
+ * Return: amount of characters printed
+ */
+int print_string(va_list list)
+{
+int i = 0, l = 0;
+char *s;
+str = va_arg(list, char *);
+if (str == NULL)
+return (0);
+while (s[i] != '\0')
+{
+l += _putchar(s[i]);
+i++;
+}
+return (l);
+}
+
+/**
+ * print_char - prints characters
+ * @list: list of arguments
+ * Return: amount of characters printed
+ */
+int print_char(va_list list)
+{
+_putchar(va_arg(list, int));
+return (1);
+}
+
+/**
+ * print_percent - prints a percent symbol
+ * @list: list of arguments
+ * Return: amount of characters printed
+ */
+int print_percent(va_list list)
+{
+int percent = va_arg(list, int);
+percent = 37;
+_putchar(percent);
+return (1);
+}
