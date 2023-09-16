f57f2479eb840c0c760e32ec98aedba60cdd099a
Mai <mai.zakaria.18@gmail.com>
2023-09-16 14:21:07 -0700

    print_integer

--- print_num.c
+++ print_num.c
@@ -6,24 +6,25 @@
  */
 int print_integer(va_list list)
 {
-	int number = va_arg(list, int);
-	int count = 0, digit, divisor = 1;
+int number = va_arg(list, int);
+int count = 0, digit, divisor = 1;
 
-	if (number < 0)
-	{
-		_putchar('-');
-		number = -number;
-	}
-	while (number / divisor > 9)
-		divisor *= 10;
-	while (divisor > 0)
-	{
-		digit = number / divisor;
-		_putchar('0' + digit);
-		number %= divisor;
-		divisor /= 10;
-		count++;
-	        
-	}
-	return (count);
+if (number < 0)
+{
+_putchar('-');
+number = -number;
+}
+while (number / divisor > 9)
+{
+divisor *= 10;
+while (divisor > 0)
+{
+digit = number / divisor;
+_putchar('0' + digit);
+number %= divisor;
+divisor /= 10;
+count++;
+}
+}
+return (count);
 }
