8c12bed0a6b08eb0f06861a5d582ac17f61bc17f
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-16 15:46:19 +0300

    header file

--- main.h
+++ main.h
@@ -6,6 +6,17 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/**
+ *struct convert - convert specifier
+ *@sym: letter
+ *@f: function
+ */
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+} convert_specs;
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_char(va_list);
