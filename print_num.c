7883515d3704082da675f70afc8c625a431ee711
Lotfy98 <mohamed.lotfy78@gmail.com>
2023-09-17 17:07:50 +0300

    m

--- print_num.c
+++ print_num.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <math.h>
 /**
  *print_integer - prints an int
  *@list: list of args
@@ -7,7 +6,7 @@
  */
 int print_integer(va_list list)
 {
-	long n = va_arg(list, int), t = n, i;
+	long n = va_arg(list, int), t = n, i, pwr;
 	int num = 0;
 
 	if (n < 0)
@@ -17,18 +16,22 @@ int print_integer(va_list list)
 	}
 	if (n == 0)
 	{
-		_putchar(48);
+		_putchar(n);
 	}
 	while (t != 0)
 	{
 		num++;
 		t /= 10;
 	}
-	i = pow(10, num - 1);
-	while (i > 0)
+	pwr = 1;
+	i = 1;
+	while (i < num)
 	{
-		_putchar(48 + (n / i) % 10);
-		i /= 10;
+		pwr *= 10;
+		i++;
 	}
+	for (i = pwr; i > 0; i /= 10)
+		_putchar(48 + (n / i) % 10);
+
 	return (num);
 }
