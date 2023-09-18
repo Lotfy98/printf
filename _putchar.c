d6b7104b57814840e70bbaa8a2a355b735bc1b79
Mai <mai.zakaria.18@gmail.com>
2023-09-18 04:36:39 -0700

    update

--- _putchar.c
+++ _putchar.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /*#define BUFFER_SIZE 1024*/
-char buffer[BUFFER_SIZE];
-int buffer_index = 0;
+
 /**
  * _putchar - write char c to stdout
  *
@@ -11,11 +10,14 @@ int buffer_index = 0;
  */
 int _putchar(char c)
 {
-	if (buffer_index >= BUFFER_SIZE - 1 || c == '\n')
+char *buffer;
+int *buffer_index;
+int buffer_size;
+	if (*buffer_index >= buffer_size || c == '\n')
 	{
-		write(1, buffer, buffer_index);
-		buffer_index = 0;
+		write(1, buffer, *buffer_index);
+		*buffer_index = 0;
 	}
-	buffer[buffer_index++] = c;
+	buffer[*buffer_index++] = c;
 	return (1);
 }
